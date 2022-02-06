12051cba3de34a678a077c66fa8eece0fc257d57
Samet <sametcoruk0604@gmail.com>
2022-02-06 14:04:00 +0300

    getSehirAd metodunu çıkardım.

--- src/main/java/com/bilgeadam/odevsehirapp/controller/SehirController.java
+++ src/main/java/com/bilgeadam/odevsehirapp/controller/SehirController.java
@@ -4,7 +4,9 @@ import com.bilgeadam.odevsehirapp.entity.Sehir;
 import com.bilgeadam.odevsehirapp.exception.SehirNotFoundException;
 import com.bilgeadam.odevsehirapp.repository.SehirRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,48 +18,46 @@ public class SehirController {
     private SehirRepository sehirRepository;
 
     @GetMapping("/sehir/{id}")
-    public Sehir getSehir(@PathVariable("id") long id) {
+    public Sehir getSehir(@PathVariable("id") long id)
+    {
         Sehir sehir = null;
         Optional<Sehir> sehirDB = sehirRepository.findById(id);
 
-        if (sehirDB.isPresent()) {
+        if(sehirDB.isPresent())
+        {
             sehir = sehirDB.get();
             return sehir;
-        } else {
-            throw new SehirNotFoundException(id + "nolu Sehir bulunamadı!");
         }
+        else
+        {
+            throw new SehirNotFoundException(id + " nolu Sehir bulunamadı!");
+        }
+
     }
 
     @GetMapping("/sehir")
-    public List<Sehir> getTumSehirler() {
+    public List<Sehir> getTumSehirler()
+    {
         return sehirRepository.findAll();
     }
 
+    @GetMapping("/sehir/ara/ad/{ad}")
+    public Sehir findSehirByAd(@PathVariable("ad") String ad)
+    {
+        Sehir sehir = sehirRepository.findByAd(ad);
+        return sehir;
+    }
+
     @GetMapping("/sehirad/{id}")
-    public String getSehirAd(@PathVariable("id") long id) {
+    public String getSehirAd(@PathVariable("id") long id)
+    {
 
         Sehir sehir = getSehir(id);
         return sehir.getAd();
 
-        //return getSehirAd(id).getAd();
-    }
-
-    @PostMapping("/sehirEkle")
-    public void addSehir(@RequestBody Sehir sehir) {
-        sehirRepository.save(sehir);
     }
 
 
-    @GetMapping("/sehir/ad/{ad}")
-    public Sehir findSehirByAd(@PathVariable("ad") String ad) {
-
-
-        {
-            Sehir sehir = sehirRepository.findByAd(ad);
-            return sehir;
-
-        }
-    }
 
 
 }
